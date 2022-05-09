b08756060e44d170b7891de990564d17d365c396
zerostyle <zerostyle@126.com>
2022-05-09 14:26:12 +0800

    测试同步到github

--- src/assets/core/ahcui/index.js
+++ src/assets/core/ahcui/index.js
@@ -1,6 +1,3 @@
-//import "@ahcui/vue3-multiplex/dist/lib/css/ahcui.multiplex.min.css"
-//export {vue, app, cuiapi, router, store, mixin, elementPlus, axios, auth, NameSpace, namespace, eventDefine, eventBus, version ,runtimeCfg} from "@ahcui/vue3-multiplex";
+import "@ahcui/vue3-multiplex/dist/lib/css/ahcui.multiplex.min.css"
+export {vue, app, cuiapi, router, store, mixin, elementPlus, axios, auth, NameSpace, namespace, eventDefine, eventBus, version ,runtimeCfg} from "@ahcui/vue3-multiplex";
 
-
-import "../../../../ahcui/static/css/main.css";
-export {vue, app, cuiapi, router, store, mixin, elementPlus, axios, auth, NameSpace, namespace, eventDefine, eventBus, version ,runtimeCfg} from "../../../../ahcui/export";
